33af910f1e4c77432bd294c4ac594806ddc71c68
Gustavo <gustavo@kapibara.com.br>
2023-10-16 08:26:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,52 @@ import fs from "fs";
 import { Blob } from "buffer";
 import Htmlifier from '@sheeptester/htmlifier'
 
-const trabalhoId = 1;
+const trabalhoId = 6;
 
 async function convert(file) {
     let buffer = fs.readFileSync(`sb3-files/${file}.sb3`);
     let blob = new Blob([buffer]);
 
-    const html = await new Htmlifier({})
+    let html = await new Htmlifier({})
         .htmlify(
             {
                 type: 'file',
                 file: blob
             },
+            //HtmlifyOptions
             {
-                includeVm: false
-            })
+                autoStart: false,
+                includeVm: false,
+                //LoadingOptions
+                loading: {
+                    progressBar: '#00ffff'
+                },
+                //ButtonOptions
+                buttons: {
+                    fullscreen: true,
+                    startStop: true
+                }
+            },
+        )
         .then(blob => blob.text());
+
+    //troca para vm local
+    html = html.replace('https://sheeptester.github.io/scratch-vm/16-9/vm.min.js', '../vm.min.js');
+
     await fs.writeFile(`html-files/${file}.html`, html, (err) => {
         if (err)
             return console.log(`Erro ao gravar arquivo (${file}): ${err}`);
     })
 }
 
-async function main() {
+async function main(trabalhoId) {
     if (!fs.existsSync('sb3-files')) {
         fs.mkdirSync('sb3-files');
     }
     if (!fs.existsSync('html-files')) {
         fs.mkdirSync('html-files');
     }
-    
+
     fs.readdir('sb3-files', function (err, files) {
         if (err) {
             return console.log('Erro ao ler arquivos: ' + err);
@@ -53,4 +69,4 @@ async function main() {
     });
 }
 
-main();
+main(trabalhoId);
